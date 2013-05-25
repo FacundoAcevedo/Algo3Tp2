e8c117d233577754a76819a5cb77cf60fd5a484f
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-25 22:23:07 +0000

    Agregada la clase lancha y su constructor

---
+++ src/juego/Lancha.java
@@ -0,0 +1,11 @@
+package juego;
+
+public class Lancha extends Nave{
+	final static int miLargo = 2;
+
+	public Lancha(Direccion direccionLancha){
+		//Llamo al constructor de Nave
+		super(miLargo, direccionLancha);
+	}
+
+}
