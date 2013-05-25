234b870fcd1e548014aa6af4b5b3735a51984293
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-25 22:32:57 +0000

    Agregada la clase Buque y su constructor

---
+++ src/juego/Buque.java
@@ -0,0 +1,11 @@
+package juego;
+
+public class Buque extends Nave{
+	final static int miLargo = 4;
+	
+	public Buque (Direccion direccionBuqe){
+		//Llamo al constructor de Nave
+		super(miLargo, direccionBuqe);
+	}
+	
+}
