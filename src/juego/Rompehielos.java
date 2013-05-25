445ca2c4b1642c77778c70d5ef3c4cc1d73e4525
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-25 22:07:33 +0000

    Creada la clase y agregado su constructor

---
+++ src/juego/Rompehielos.java
@@ -0,0 +1,13 @@
+package juego;
+
+public class Rompehielos extends Nave{
+	final static int miLargo = 3;
+	
+	public Rompehielos(Direccion direccionNave){
+		//Llamo al constructor de Nave
+		super(miLargo, direccionNave);
+		
+	}
+	
+
+}
