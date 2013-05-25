1a3b6b1913b5071ffd41f01d8e1a6a944a1bf413
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-25 22:30:21 +0000

    agregada la clase Destructor y su constructor

---
+++ src/juego/Destructor.java
@@ -0,0 +1,12 @@
+package juego;
+
+public class Destructor extends Nave{
+	final static int miLargo = 3;
+
+	public Destructor(Direccion direccionDestructor){
+		//Llamo al constructor de Nave
+		super(miLargo, direccionDestructor);
+	}
+	
+
+}
