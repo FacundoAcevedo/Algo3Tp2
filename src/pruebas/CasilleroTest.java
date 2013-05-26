5806401d6f7465b74bf6285899fbea85cd7a649a
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-26 18:34:41 +0000

    -Refactor -Agregados los test para instanciacion de un casillero usando un array

--- src/pruebas/CasilleroTest.java
+++ src/pruebas/CasilleroTest.java
@@ -15,6 +15,12 @@ public class CasilleroTest {
         assertTrue(casillero != null);
 	}
 	@Test
+    public void testCrearConIdArray() {
+		int[] id = {0,0};
+        Casillero casillero = new Casillero(id);
+        assertTrue(casillero != null);
+	}
+	@Test
     public void testCasilleroObtenerIdDiagonalInferior(){
 	 Casillero casillero = new Casillero(0,0);
 	 int[] id = {0,0};
@@ -23,9 +29,23 @@ public class CasilleroTest {
 	
 	@Test
     public void testCasilleroObtenerIdDiagonalSuperior(){
-	 Casillero casillero = new Casillero(99,99);
-	 int[] id = {99,99};
+	 Casillero casillero = new Casillero(9,9);
+	 int[] id = {9,9};
 	 assertArrayEquals(casillero.id(), id);
+	}
+	
+	@Test
+    public void testCasilleroObtenerIdDiagonalInferiorCreadoConIdArray(){
+		int[] id = {0,0};
+    	Casillero casillero = new Casillero(id);
+    	assertArrayEquals(casillero.id(), id);
+	}
+	
+	@Test
+    public void testCasilleroObtenerIdDiagonalSuperiorCreadoConIdArray(){
+		int[] id = {9,9};
+    	Casillero casillero = new Casillero(id);
+    	assertArrayEquals(casillero.id(), id);
 	}
 	
 	@Test(expected=ErrorIdCasilleroInvalido.class)
@@ -34,7 +54,7 @@ public class CasilleroTest {
 	}
 	
 	@Test(expected=ErrorIdCasilleroInvalido.class)
-    public void testCasilleroObtenerIdSuperiorAnoventaYNueve(){
+    public void testCasilleroObtenerIdSuperiorANueve(){
 	 new Casillero(100,123);
 	}
 	
@@ -46,6 +66,17 @@ public class CasilleroTest {
 	@Test(expected=ErrorIdCasilleroInvalido.class)
     public void testCasilleroObtenerIdYInvalido(){
 	 new Casillero(5,100);
+	}
+	@Test(expected=ErrorIdCasilleroInvalido.class)
+    public void testCasilleroObtenerIdXInvalidoConIdArray(){
+	 int[] id = {111,5};
+	 new Casillero(id);
+	}
+	
+	@Test(expected=ErrorIdCasilleroInvalido.class)
+    public void testCasilleroObtenerIdYInvalidoConIdArray(){
+	 int[] id = {5,111};
+	 new Casillero(id);
 	}
 	
 	
