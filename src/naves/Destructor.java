9d61f4e2db64037720774426e7a0a73895383e68
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-26 19:43:45 +0000

    Reescrito vulnerable() para esta clase

--- src/naves/Destructor.java
+++ src/naves/Destructor.java
@@ -1,11 +1,19 @@
 package naves;
 
+import municiones.*;
 public class Destructor extends Nave{
 	final static int miLargo = 3;
 
 	public Destructor(Direccion direccionDestructor){
 		//Llamo al constructor de Nave
 		super(miLargo, direccionDestructor);
+	}
+
+	public  boolean vulnerable(Municion municion){
+		if (municion instanceof DisparoConvecional){
+			return true;
+		}
+		return false;
 	}
 	
 
