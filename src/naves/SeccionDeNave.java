045b8afe8164f83e6284f5cf3c262428e5f2db79
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-26 19:13:16 +0000

    Cada seccion de la nave, al recibir un disparo, actualizara el estado general de la nave

--- src/naves/SeccionDeNave.java
+++ src/naves/SeccionDeNave.java
@@ -1,17 +1,21 @@
 package naves;
 
 public class SeccionDeNave {
-	protected EstadoSeccion estado;
+	private EstadoDeSalud estado;
+	private Nave nave; //Referencia a la nave donde pertenece esta seccion
 	
-	public SeccionDeNave() {
-		this.estado = EstadoSeccion.SANO;
+	public SeccionDeNave(Nave nave) {
+		this.nave = nave;
+		this.estado = EstadoDeSalud.ACTIVO;
 	}
 	
-	public void recibirImpacto(){
-		this.estado = EstadoSeccion.DESTRUIDO;
+	public void destruir(){
+		this.estado = EstadoDeSalud.DESTRUIDO;
+		this.nave.actualizar();
 	}
 	
-	public EstadoSeccion estado(){
+	
+	public EstadoDeSalud estado(){
 		return this.estado;
 	}
 	
