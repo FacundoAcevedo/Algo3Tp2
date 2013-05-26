78dd60d17910ee4794f0a448d53d5d67ccade9e2
FacundoAcevedo <acv2facundo@gmail.com>
2013-05-26 19:26:44 +0000

    Agregada el metodo recibirImpacto()

--- src/naves/Buque.java
+++ src/naves/Buque.java
@@ -1,4 +1,5 @@
 package naves;
+import municiones.*;
 
 public class Buque extends Nave{
 	final static int miLargo = 4;
@@ -6,6 +7,19 @@ public class Buque extends Nave{
 	public Buque (Direccion direccionBuqe){
 		//Llamo al constructor de Nave
 		super(miLargo, direccionBuqe);
+	}
+	
+	public void recibirImpacto( Municion municion){
+		//Cualquier tipo de municion recibida en alguna de sus secciones,
+		//La destruye por completo
+		
+		//Destruyo c/u de las partes
+		for(int i = 0; i <= this.largo; i++){
+			SeccionDeNave seccion = this.secciones.get(i);
+			seccion.destruir();		
+		}
+		this.actualizar(); 
+		
 	}
 	
 }
