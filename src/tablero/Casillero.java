16951e95371afc35f694b63b960c69b3b0cb0226
Martin Promet <martinpromet@hotmail.com>
2013-06-01 23:05:35 -0300

    Se creo un nuevo constructor. Hay que decidir bien la estrategia, si usamos IdCasillero o int[] id

--- src/tablero/Casillero.java
+++ src/tablero/Casillero.java
@@ -5,15 +5,21 @@ import naves.SeccionDeNave;
 import municiones.Municion;
 
 public class Casillero {
-	IdCasillero id;
+	IdCasillero id; //¿Lo definimos como IdCasillero pero se implementa como int[]? Hay algo raro
 	ContenidoCasillero contenido;
 	
 	public Casillero(int x, int y){
 		this.id = new	IdCasillero(x, y);
 		this.contenido = new ContenidoCasillero(); 
 	}
+	
 	public Casillero(int [] id){
 		this(id[0],id[1]); 
+	}
+	
+	// Hay que definir como usar el id
+	public Casillero(IdCasillero id) {
+		this.id = id;
 	}
 	
 	public void ponerSeccionDeNave(SeccionDeNave seccionDeNave){
@@ -33,6 +39,8 @@ public class Casillero {
 		return this.contenido.devolverMuniciones();
 	}
 	
+	
+	// ¿No deberia devolver IdCasillero en lugar de int[]?
 	public int[] id(){
 		return this.id.id();
 	}
