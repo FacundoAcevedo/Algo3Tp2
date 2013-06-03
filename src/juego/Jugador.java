47ad4f9f6659006c076c997208b3970e444c5201
deniseck <denhjskf@gmail.com>
2013-06-03 03:21:19 -0300

    implementé método disparar

--- src/juego/Jugador.java
+++ src/juego/Jugador.java
@@ -1,7 +1,10 @@
 package juego;
 
+import municiones.Municion;
+import tablero.Casillero;
+
 public class Jugador {
-	protected Puntaje puntaje;
+	private Puntaje puntaje;
 	
 	public Jugador(){
 		this.puntaje = new Puntaje();
@@ -14,5 +17,9 @@ public class Jugador {
 	public void descontarPuntos(int puntosPerdidos) {
 		puntaje.descontarPuntos(puntosPerdidos);
 	}
-	
+
+	public void disparar(Municion municion, Casillero casillero){
+		casillero.ponerMunicion(municion);
+		this.descontarPuntos(municion.costo()); //10 es la cantidad de puntos que se pierden por turno. Ver si es mejor definirlo en otro lado.
+	}
 }
