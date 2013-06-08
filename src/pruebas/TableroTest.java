6d15984b5a7f058ef4fa3d78293479202a753735
FacundoAcevedo <acv2facundo@gmail.com>
2013-06-08 16:25:43 +0200

    Agregados los test para PatronDeSuma, y buscarCasilleroParaProa

--- src/pruebas/TableroTest.java
+++ src/pruebas/TableroTest.java
@@ -1,11 +1,13 @@
 package pruebas;
 
-
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.LinkedList;
 
 import naves.Nave;
+import naves.Sentido;
 
 import org.junit.Test;
 
@@ -18,44 +20,107 @@ public class TableroTest {
 	@Test
 	public void testCrear() {
 		Tablero tablero = new Tablero();
-        assertTrue(tablero != null);
+		assertTrue(tablero != null);
 	}
-	
+
 	@Test
 	public void testSeCreaSinNaves() {
 		Tablero tablero = new Tablero();
-        assertFalse(tablero.tieneNaves());
+		assertFalse(tablero.tieneNaves());
 	}
+
 	@Test
 	public void testPonerNavesAleatoriamente() {
-		//Se testea la existencia de las naves, y su tipo
-		//no la aleatoreidad de sus posiciones.
+		// Se testea la existencia de las naves, y su tipo
+		// no la aleatoreidad de sus posiciones.
 		Tablero tablero = new Tablero();
-        tablero.posicionarNavesAleatoriamente();
-        assertTrue(tablero.cantidadTotalNaves() == 7);
-        LinkedList<Nave> naves = tablero.devolverNaves();
-        
-        for(Nave nave : naves){
-        	assertTrue(nave instanceof Nave);
-        }
+		tablero.posicionarNavesAleatoriamente();
+		assertTrue(tablero.cantidadTotalNaves() == 7);
+		LinkedList<Nave> naves = tablero.devolverNaves();
+
+		for (Nave nave : naves) {
+			assertTrue(nave instanceof Nave);
+		}
 	}
-	
+
 	@Test
 	public void testPedirCasilleroValido() {
 		Tablero tablero = new Tablero();
-		int [] id = {0,0};
-        Casillero casillero = tablero.obtenerCasillero(id);
-        assertTrue(casillero instanceof Casillero);
+		int[] id = { 0, 0 };
+		Casillero casillero = tablero.obtenerCasillero(id);
+		assertTrue(casillero instanceof Casillero);
 	}
-	
-	@Test(expected=ErrorIdCasilleroInvalido.class)
+
+	@Test(expected = ErrorIdCasilleroInvalido.class)
 	public void testPedirCasilleroInvalido() {
 		Tablero tablero = new Tablero();
-		int [] id = {-10,10};
+		int[] id = { -10, 10 };
 		tablero.obtenerCasillero(id);
 
 	}
-	
 
+	@Test
+	public void patronDeSumaParaUbicarNaveSur() throws NoSuchMethodException,
+			IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException {
+
+		// pruebas por reflexion
+		Tablero tablero = new Tablero();
+		Sentido unSentido = Sentido.SUR;
+		int[] patronSur = { 0, -1 };
+
+		Method patronDeSuma = Tablero.class.getDeclaredMethod(
+				"patronDeSumaParaUbicarNave", Sentido.class);
+		patronDeSuma.setAccessible(true);
+
+		int[] patron = (int[]) patronDeSuma.invoke(tablero, unSentido);
+
+		assertTrue(patron[0] == patronSur[0] && patron[1] == patronSur[1]);
+	}
+
+	@Test
+	public void patronDeSumaParaUbicarNaveNoreste()
+			throws NoSuchMethodException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+
+		// pruebas por reflexion
+		Tablero tablero = new Tablero();
+		Sentido unSentido = Sentido.NORESTE;
+		int[] patronNorEste = { 1, 1 };
+
+		Method patronDeSuma = Tablero.class.getDeclaredMethod(
+				"patronDeSumaParaUbicarNave", Sentido.class);
+		patronDeSuma.setAccessible(true);
+
+		int[] patron = (int[]) patronDeSuma.invoke(tablero, unSentido);
+
+		assertTrue(patron[0] == patronNorEste[0]
+				&& patron[1] == patronNorEste[1]);
+	}
+
+	@Test
+	public void buscarCasilleroParaProa() throws NoSuchMethodException,
+			IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException {
+
+		// Se testea que este dentro de los limites de seguridad para la
+		// ubicacion de naves
+		Tablero tablero = new Tablero();
+		int limiteInferior = 3;
+		int limiteSuperior = 6;
+
+		Method buscarCasilleroParaProa = Tablero.class
+				.getDeclaredMethod("buscarCasilleroParaProa");
+		buscarCasilleroParaProa.setAccessible(true);
+
+		for (int i = 0; i < 10; i++) {
+			int[] idCasillero = (int[]) buscarCasilleroParaProa.invoke(tablero);
+
+			assertTrue(idCasillero[0] > limiteInferior
+					&& idCasillero[0] < limiteSuperior
+					&& idCasillero[1] > limiteInferior
+					&& idCasillero[1] < limiteSuperior);
+		}
+	}
 
 }
