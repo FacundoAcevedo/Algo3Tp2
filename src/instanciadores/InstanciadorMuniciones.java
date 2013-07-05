180d0d97a9ad6084ba8a5d2f40d54f2b5fcacdd5
FacundoAcevedo <acv2facundo@gmail.com>
2013-07-04 21:07:59 -0300

    Creada la clase encargada de instanciar municiones

---
+++ src/instanciadores/InstanciadorMuniciones.java
@@ -0,0 +1,34 @@
+package instanciadores;
+
+import municiones.DisparoConvencional;
+import municiones.MinaSubmarinaDobleConRetardo;
+import municiones.MinaSubmarinaPorContacto;
+import municiones.MinaSubmarinaPuntualConRetardo;
+import municiones.MinaSubmarinaTripleConRetardo;
+
+public class InstanciadorMuniciones {
+	//Clase encargada de devolver un objeto a partir de una cadena
+	
+	public Object instanciardor(String nombreDelObjeto){
+		if(nombreDelObjeto == "Disparo Convencional"){
+			return new DisparoConvencional();
+			
+		}
+		else if(nombreDelObjeto == "Mina Por Contacto"){
+			return new MinaSubmarinaPorContacto();
+		}
+		else if(nombreDelObjeto == "Mina Puntual Con Retardo" ){
+			return new MinaSubmarinaPuntualConRetardo();
+		}
+		else if(nombreDelObjeto == "Mina Doble Con Retardo"){
+			return new MinaSubmarinaDobleConRetardo();
+		}
+		else{
+			//"Mina Triple Con Retardo"
+			return new MinaSubmarinaTripleConRetardo();
+			
+		}
+		
+	}
+
+}
