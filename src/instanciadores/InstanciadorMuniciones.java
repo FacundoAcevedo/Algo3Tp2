8c79ca3692528d20c62d5dd6ce4ecd42613665b7
FacundoAcevedo <acv2facundo@gmail.com>
2013-07-04 21:12:56 -0300

    ya esta, estatico ( me di cuenta despues de comitear todo :P)

--- src/instanciadores/InstanciadorMuniciones.java
+++ src/instanciadores/InstanciadorMuniciones.java
@@ -10,7 +10,7 @@ import municiones.Municion;
 public class InstanciadorMuniciones {
 	//Clase encargada de devolver un objeto a partir de una cadena
 	
-	public Municion instanciardor(String nombreDelObjeto){
+	final public Municion instanciardor(String nombreDelObjeto){
 		if(nombreDelObjeto == "Disparo Convencional"){
 			return new DisparoConvencional();
 			
