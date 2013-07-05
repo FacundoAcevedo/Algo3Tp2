b681c8865f8c0dfd8f7e1600393a30b13139d144
FacundoAcevedo <acv2facundo@gmail.com>
2013-07-04 21:10:16 -0300

    Se le corrigio el tipo que devolvia

--- src/instanciadores/InstanciadorMuniciones.java
+++ src/instanciadores/InstanciadorMuniciones.java
@@ -5,11 +5,12 @@ import municiones.MinaSubmarinaDobleConRetardo;
 import municiones.MinaSubmarinaPorContacto;
 import municiones.MinaSubmarinaPuntualConRetardo;
 import municiones.MinaSubmarinaTripleConRetardo;
+import municiones.Municion;
 
 public class InstanciadorMuniciones {
 	//Clase encargada de devolver un objeto a partir de una cadena
 	
-	public Object instanciardor(String nombreDelObjeto){
+	public Municion instanciardor(String nombreDelObjeto){
 		if(nombreDelObjeto == "Disparo Convencional"){
 			return new DisparoConvencional();
 			
