713a98c7b440a70bf361f51479ee667dfcd4a8b2
deniseck <denhjskf@gmail.com>
2013-07-08 19:34:15 -0300

    Refactor

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -78,10 +78,10 @@ public class Controlador {
 			municionSeleccionada = (String) listaMuniciones.getSelectedValue();
 			
 			informacionDeMuniciones.put("Disparo Convencional","Impacta en una posición en el mismo momento del disparo" + "\n" + "Costo: 200 puntos");
-			informacionDeMuniciones.put("Mina Por Contacto","El impacto se realiza cuando una nave pasa por esa posición. Costo: 150 puntos");
-			informacionDeMuniciones.put("Mina Puntual Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición. Costo: 50 puntos");
-			informacionDeMuniciones.put("Mina Doble Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición. Destruye la posición actual y las posiciones adyacentes con radio 1. Costo: 100 puntos");
-			informacionDeMuniciones.put("Mina Triple Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición. Destruye la posición actual y las posiciones adyacentes con radio 2. Costo: 150 puntos");
+			informacionDeMuniciones.put("Mina Por Contacto","El impacto se realiza cuando una nave pasa por esa posición" + "\n" + "Costo: 150 puntos");
+			informacionDeMuniciones.put("Mina Puntual Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición" + "\n" + "Costo: 50 puntos");
+			informacionDeMuniciones.put("Mina Doble Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición. Destruye la posición actual y las posiciones adyacentes con radio 1" + "\n" + "Costo: 100 puntos");
+			informacionDeMuniciones.put("Mina Triple Con Retardo","Impacta tres turnos después de haber sido colocado en esa posición. Destruye la posición actual y las posiciones adyacentes con radio 2" + "\n" + "Costo: 150 puntos");
 			
 			vista.informacionMunicion.setText((String)informacionDeMuniciones.get(municionSeleccionada));
 		}
