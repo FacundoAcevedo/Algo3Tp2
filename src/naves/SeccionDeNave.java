0a3d39c66e8c6fc28c1f5e97af844c61109d32cc
Maxi <maxipenia@hotmail.com>
2013-07-08 01:21:19 -0300

    Agregado TipoDeSeccion a SeccionDeNave

--- src/naves/SeccionDeNave.java
+++ src/naves/SeccionDeNave.java
@@ -3,16 +3,19 @@ import municiones.Municion;
 public class SeccionDeNave {
 	private EstadoDeSalud estado;
 	private Nave nave; //Referencia a la nave donde pertenece esta seccion
+	private TipoDeSeccion tipoDeSeccion;
 	
-	public SeccionDeNave(Nave nave) {
+	public SeccionDeNave(Nave nave, TipoDeSeccion tipoDeSeccion) {
 		this.nave = nave;
 		this.estado = EstadoDeSalud.SANO;
+		this.tipoDeSeccion = tipoDeSeccion;
 	}
 	
 	public void destruir(){
 		this.estado = EstadoDeSalud.DESTRUIDO;
 		this.nave.actualizarEstado();
 	}
+	
 	public void recibirImpacto(Municion municion){
 		EstadoDeSalud gradoDeVulnerabilidad = this.nave.vulnerable(municion);
 		
@@ -33,14 +36,30 @@ public class SeccionDeNave {
 		}
 		
 	}
+	
 	public Sentido sentido(){
 		return this.nave.direccion();
+	}
+	
+	private boolean esPopa(){
+		return (this.tipoDeSeccion == TipoDeSeccion.POPA);
+	}
+	
+	private boolean esProa(){
+		return (this.tipoDeSeccion == TipoDeSeccion.PROA);
+	}
+	
+	public void invertirPopaProa(){
+		if (this.esProa()){
+			this.tipoDeSeccion = TipoDeSeccion.POPA;
+		}else if(this.esPopa()){
+			this.tipoDeSeccion = TipoDeSeccion.PROA;
+		}
 	}
 	
 	public void invertirSentido(){
 		this.nave.invertirSentido();
 	}
-	
 	
 	public EstadoDeSalud estado(){
 		return this.estado;
@@ -50,7 +69,8 @@ public class SeccionDeNave {
 		return this.nave.getClass().getSimpleName();
 	}
 	
+	public TipoDeSeccion obtenerTipoDeSeccion(){
+		return this.tipoDeSeccion;
+	}
 	
-	
-
 }
