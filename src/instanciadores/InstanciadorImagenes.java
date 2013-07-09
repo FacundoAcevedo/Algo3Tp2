1ada4669698e7aed5ed71a8af4044c5a1a179379
FacundoAcevedo <acv2facundo@gmail.com>
2013-07-09 00:21:17 -0300

    Carga las imagenes, tengo que pegarle una revisada

--- src/instanciadores/InstanciadorImagenes.java
+++ src/instanciadores/InstanciadorImagenes.java
@@ -4,13 +4,14 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
+import naves.EstadoDeSalud;
 import naves.SeccionDeNave;
 import naves.Sentido;
 
 public class InstanciadorImagenes {
 	//Instancia las imagenes segun el tipo
 	
-	static public Image nave(SeccionDeNave seccion){
+	static public ImageIcon nave(SeccionDeNave seccion){
 		//Se fija que el tipo de seccion, en donde esta ubicada(proa, popa, etc), el sentido y el estado
 		//Va formando el nombre de la imagen segun su estado
 		String tipo;
@@ -21,37 +22,70 @@ public class InstanciadorImagenes {
 		
 		String rutaImagen;
 		
+//				V= vertical
+//				U = up
+//				H= horizontal
+//				D= diagonal
+//				R= right
+//				L =left
+//				D= down
 		
 		//Obtengo el tipo:
-		tipo = seccion.obtenerTipo();
+		tipo = seccion.obtenerTipoDeNave();
 		
 		//Obtengo la ubicacion de la seccion
-			//TODO: implementar en tablero y otros.
+		if (!seccion.esProa() && !seccion.esPopa()){
+			posicion = "seccionMedia";
+		}
+		else if (seccion.esProa()){
+			posicion = "proa";
+		}
+		else{ //popa
+			posicion = "popa";
+		}
+		
 		
 		//obtengo el sentido, solo me importa si esta en dianogan o vertical/horizontal.
+		
 		sentidoPreProcesado = seccion.sentido();
-		if (sentidoPreProcesado == Sentido.ESTE || sentidoPreProcesado == Sentido.OESTE){
-			sentido = "horizontal";
+		if (sentidoPreProcesado == Sentido.OESTE){
+			sentido = "HL";
 		}
-		else if (sentidoPreProcesado == Sentido.NORESTE || sentidoPreProcesado == Sentido.SUDOESTE){
-			sentido = "diagonalSudoesteNoreste";
+		if (sentidoPreProcesado == Sentido.ESTE){
+			sentido = "HR";
 		}
-		else if (sentidoPreProcesado == Sentido.NOROESTE || sentidoPreProcesado == Sentido.SUDESTE){
-			sentido = "diagonalNoroesteSudeste";
+		else if (sentidoPreProcesado == Sentido.NORESTE){
+			sentido = "DRU";
+		}
+		else if (sentidoPreProcesado == Sentido.NOROESTE){
+			sentido = "DLU";
+		}
+		else if (sentidoPreProcesado == Sentido.SUDESTE){
+			sentido = "DLD";
+		}
+		else if (sentidoPreProcesado == Sentido.SUDOESTE){
+			sentido = "DLD";
+		}
+		else if(sentidoPreProcesado == Sentido.SUR){
+			sentido = "VD";
+		}
+		else {//NORTE
+			sentido = "VU";
+		}
+				
+		//obtengo el estado:
+		
+		if(seccion.estado() == EstadoDeSalud.SANO){
+			estado = "Sana";
 		}
 		else{
-			// norte o sur
-			sentido = "vertical";
+			estado = "Daniado";
 		}
-		sentido = seccion.sentido().toString();
 		
-		//obtengo el estado:
-		estado = seccion.estado().toString();
-		
-		rutaImagen = "estaticos/"+tipo+"_"+estado+"_"+sentido+"_"+posicion;
+		rutaImagen = "estaticos/Naves/"+posicion+estado+sentido+".png";
 		//instancio la imagen
 		
-		return  new ImageIcon(rutaImagen).getImage();		
+		return  new ImageIcon(rutaImagen);//.getImage();		
 	}
 //	
 //	static public Image municion(Municion municion){
