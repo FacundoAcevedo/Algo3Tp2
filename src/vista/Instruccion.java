de03ea3419ab0340785f24deca277c7f5656ecc9
deniseck <denhjskf@gmail.com>
2013-07-11 20:07:35 -0300

    modificado un comentario

--- src/vista/Instruccion.java
+++ src/vista/Instruccion.java
@@ -28,7 +28,7 @@ import javax.swing.border.Border;
        		"-Disparos: distinta cantidad de putos segun el disparo"+"\n"+"\n"+ "Naves:"+ "\n"+
        				"-2 lanchas"+"\n"+"-2 destructores: solo lo impactan los disparos directos"+"\n"+
        		"-1 buque: cualquier impacto en cualquier parte lo destruye por completo"+"\n"+
-       				"-1 portaaviones" + "\n" + "1 rompehielos: Cada parte requiere dos impactos para ser destruida";
+       				"-1 portaaviones" + "\n" + "-1 rompehielos: Cada parte requiere dos impactos para ser destruida";
        
        final ImageIcon imageIcon = new ImageIcon("estaticos/Fondos/fondoInstrucciones.jpg");
        areaDeInformacion = new JTextArea(){
